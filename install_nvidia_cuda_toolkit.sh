f3ffbfa487c65c5296ec9b4d9424e6104e23090c
aigc <aigc@aigc-pc>
2025-05-13 19:39:54 +0800

    install cuda toolkit 12-9

--- install_nvidia_cuda_toolkit.sh
+++ install_nvidia_cuda_toolkit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 #安装NVIDIA的编码器
-sudo apt-get install nvidia-cuda-toolkit
+# sudo apt-get install nvidia-cuda-toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-9
+
 sudo apt install -y libnvidia-compute-550
